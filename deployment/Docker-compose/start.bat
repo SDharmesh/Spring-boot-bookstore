aa347f39b340609c3647f9eebbe7a44a851d61e9
ritik <ritiksharma232000@gmail.com>
2025-07-19 01:45:17 +0530

    improved batch file

--- deployment/Docker-compose/start.bat
+++ deployment/Docker-compose/start.bat
@@ -1,4 +1,14 @@
 @echo off
 echo Starting Docker Compose Services.....
 
+
+cd ../../catalog-service
+call mvn clean install -DskipTests
+
+cd ../deployment/docker-compose
+
+echo Dockerizing files now...
 docker-compose -f infra.yml -f apps.yml up --build -d
+
+echo All done!
+pause
