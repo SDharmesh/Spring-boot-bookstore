d57758e16d33cb8fc262a8a8f43dad57e7dd10e4
ritik <ritiksharma232000@gmail.com>
2025-07-19 18:09:18 +0530

    added maven wrapper to order service..!

---
+++ order-service/src/test/java/com/dharmesh/bookstore/orderservice/ConfigContainer.java
@@ -0,0 +1,24 @@
+package com.dharmesh.bookstore.orderservice;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class ConfigContainer {
+
+    @Bean
+    @ServiceConnection
+    KafkaContainer kafkaContainer() {
+        return new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.9.1"));
+    }
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
+}
---
+++ order-service/src/test/java/com/dharmesh/bookstore/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dharmesh.bookstore.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+class OrderServiceApplicationTests extends AbstractIT{
+
+    @Test
+    void contextLoads() {}
+}
